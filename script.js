6a52fbb491415b703f9f60efbef97a35e5876ef3
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-05-30 01:04:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
-const line = document.getElementById("line");
-let rotation = 0;
+ const line = document.getElementById('line');
+    let rotation = 0;
 
-function rotateLine() {
-	rotation += 1;
-	 line.style.transform = `rotate(${rotation}deg)`;
-	 requestAnimationFrame(rotateLine);
+    function rotateLine() {
+      rotation += 1;
+      line.style.transform = `rotate(${rotation}deg)`;
+
+      requestAnimationFrame(rotateLine);
     }
 
     rotateLine();
-
